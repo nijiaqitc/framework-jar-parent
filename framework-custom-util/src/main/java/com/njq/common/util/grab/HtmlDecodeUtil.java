8ad510b8d9ed9639b689513fb4a0be518a74f90e
nijiaqi <583522219@qq.com>
2019-02-01 18:24:21 +0800

    抛出异常

--- framework-custom-util/src/main/java/com/njq/common/util/grab/HtmlDecodeUtil.java
+++ framework-custom-util/src/main/java/com/njq/common/util/grab/HtmlDecodeUtil.java
@@ -7,24 +7,19 @@ import java.io.FileReader;
 
 public class HtmlDecodeUtil {
 
-    public static String decodeHtml(String html, String fileJs, String fun) {
-        try {
-            ScriptEngineManager manager = new ScriptEngineManager();
-            ScriptEngine engine = manager.getEngineByName("js");
-            // 执行指定脚本
-            FileReader reader = new FileReader(fileJs);
-            engine.eval(reader);
-            if (engine instanceof Invocable) {
-                // 调用merge方法，并传入两个参数
-                Invocable invoke = (Invocable) engine;
-                // 调用了js的fun方法
-                String c = (String) invoke.invokeFunction(fun, html);
-                return c;
-            }
-            return "";
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            return "";
+    public static String decodeHtml(String html, String fileJs, String fun) throws Exception{
+        ScriptEngineManager manager = new ScriptEngineManager();
+        ScriptEngine engine = manager.getEngineByName("js");
+        // 执行指定脚本
+        FileReader reader = new FileReader(fileJs);
+        engine.eval(reader);
+        if (engine instanceof Invocable) {
+            // 调用merge方法，并传入两个参数
+            Invocable invoke = (Invocable) engine;
+            // 调用了js的fun方法
+            String c = (String) invoke.invokeFunction(fun, html);
+            return c;
         }
+        return "";
     }
 }
