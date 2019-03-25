9c7fe61bb919a478fba45319c520e17f7cb85148
nijiaqi <583522219@qq.com>
2019-03-25 13:42:27 +0800

    sdsdsdsdsdasasnmn

---
+++ framework-common-enumreg/src/main/java/com/njq/common/enumreg/FileType.java
@@ -0,0 +1,46 @@
+package com.njq.common.enumreg;
+
+import com.njq.common.enumreg.channel.ChannelType;
+import org.apache.commons.lang3.EnumUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public enum FileType implements ValueDescription {
+    IMAGE("image", "图片");
+    private String value;
+    private String description;
+    private static List<ChannelType> VALUES = EnumUtils.getEnumList(ChannelType.class);
+
+    FileType(String value, String description) {
+        this.value = value;
+        this.description = description;
+    }
+
+    public static ChannelType getChannelType(String value) {
+        return EnumHelper.getEnum(VALUES, ChannelType.class, value);
+    }
+
+    @Override
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * 获取值列表
+     *
+     * @return
+     */
+    public static List<String> getChannelValueList() {
+        List<String> valueList = new ArrayList<>();
+        VALUES.forEach(n -> {
+            valueList.add(n.getValue());
+        });
+        return valueList;
+    }
+}
